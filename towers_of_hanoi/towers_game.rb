85f076a6c44058de295e9f96287702684e7818f3
tkoh13 <tylerkoh93@gmail.com>
2021-09-02 17:35:06 -0400

    towers skeleton

---
+++ towers_of_hanoi/towers_game.rb
@@ -0,0 +1,18 @@
+class tower
+    def initialize(num_disks)
+        @num_disks = num_disks
+        @towers = [Array.new(num_disks),[],[]]
+    end
+
+    #move
+
+    #won?
+
+    #valid_move?
+
+    #play
+    #until won? 
+
+    #display
+
+end
