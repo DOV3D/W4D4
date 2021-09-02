5ca9c6e0415926275d3746bec6e8b178fc046947
tkoh13 <tylerkoh93@gmail.com>
2021-09-02 17:28:00 -0400

    started Towers of Hanoi

--- TDD/lib/tdd.rb
+++ TDD/lib/tdd.rb
@@ -21,17 +21,17 @@ end
 
 class Array
     def my_transpose
-        self.transpose
-        # dim = self.first.count
-        # raise "IndexError" if !self.all? {|row| row.length == dim}
+        # self.transpose
+        dim = self.first.count
+        raise "IndexError" if !self.all? {|row| row.length == dim}
 
-        # columns = Array.new(dim) { Array.new(dim) }
-        # (0...dim).each do |i|
-        #     (0...dim).each do |j|
-        #         columns[j][i] = self[i][j] 
-        #     end
-        # end
-        # columns
+        columns = Array.new(dim) { Array.new(dim) }
+        (0...dim).each do |i|
+            (0...dim).each do |j|
+                columns[j][i] = self[i][j] 
+            end
+        end
+        columns
     end
 end
 
@@ -51,4 +51,25 @@ def stock_picker(prices)
     max = profits.max
     index = profits.index(max)
     buy_sell[index]
+end
+
+
+# class Array
+#     def my_rotate
+#         self.rotate
+#     end
+# end
+
+class Array
+  # Define a method `Array#my_rotate(positions)` that rotates the elements 
+  # correctly based on the argument provided.  The work for positive and 
+  # negative integer arguments.  You CANNOT use Ruby's `Array#rotate` or 
+  # `Array#rotate!`.
+
+  def my_rotate(positions = 1)
+    split_idx = positions % self.length
+
+    self.drop(split_idx) + self.take(split_idx)
+    self.rotate
+  end
 end
