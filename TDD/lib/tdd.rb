f07650ec531d087c2c3a7a46738a8aef8e543ac0
DOV3D <davidovelazquez3@gmail.com>
2021-09-02 14:42:25 -0400

    Add two_sum spec

--- TDD/lib/tdd.rb
+++ TDD/lib/tdd.rb
@@ -7,3 +7,13 @@ class Array
        uniq
     end
 end
+
+def two_sum(arr)
+    new_arr = []
+    (0...arr.length).each do |i|
+        (i+1...arr.length).each do |j|
+            new_arr << [i, j] if arr[i] + arr[j] == 0
+        end
+    end
+    new_arr
+end
--- TDD/spec/tdd_spec.rb
+++ TDD/spec/tdd_spec.rb
@@ -16,3 +16,18 @@ describe Array do
         end
     end
 end
+
+describe "#two_sum" do
+    let(:same_idx_arr) {[5, 5, -5, -5]}
+    let(:arr) {[-1, 0, 2, -2, 1]}
+    let(:zero_arr) {[3, 1, 0, 2]}
+    it "finds all zero-sum pairs" do
+        expect(two_sum(arr)).to eq([[0,4],[2,3]])
+    end
+    it "can handle zero values" do
+        expect(two_sum(zero_arr)).to eq([])
+    end
+    it "finds zero-sum pairs with the same element" do
+        expect(two_sum(same_idx_arr)).to eq([[0, 2],[0, 3],[1, 2],[1, 3]])
+    end
+end
